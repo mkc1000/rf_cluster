3ea3eefe227bcecd08576f5340e50061ed6b7b71
Michael Cohen <mkc1000@gmail.com>
2016-11-15 15:25:33 -0800

    change consistency params

--- mutual_info_cluster.py
+++ mutual_info_cluster.py
@@ -27,3 +27,27 @@ class MaxMutualInfo(object):
                 mutual_infos.append(weighted_mean_mi)
             best = np.argmax(np.array(mutual_infos))
             self.assignments[point] = best
+
+class VotingCombination(object):
+    def __init__(self, k, max_iter=30):
+        self.k = k
+        self.max_iter = max_iter
+        self.data = None
+        self.weights = None
+        self.assignments = None
+
+    def fit(self, data):
+        self.data, self.weights = data
+        n_points = self.data.shape[0]
+        self.assignments = np.random.randint(0,self.k,(n_points,))
+        for _ in xrange(n_points*self.max_iter):
+            point = np.random.randint(0,n_points)
+            def vote(col,point,assignments):
+                votes = assignments[col==col[point]]
+                vote = np.argmax(np.array([np.sum(votes==i) for i in xrange(self.k)]))
+                return vote
+            votes = np.apply_along_axis(lambda col: vote(col,point,self.assignments),0,self.data)
+            vote_counts = [0 for _ in xrange(self.k)]
+            for vote, weight in zip(votes, self.weights):
+                vote_counts[vote] += weight
+            self.assignments[point] = np.argmax(vote_counts)
