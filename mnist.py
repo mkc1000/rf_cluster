a4ec80098515f6cc97f4655598c92d25f5b64d7e
Michael Cohen <mkc1000@gmail.com>
2016-11-14 19:41:38 -0800

    change mnist params

--- mnist.py
+++ mnist.py
@@ -9,17 +9,17 @@ if __name__ == '__main__':
     X = mnist.data
     numerals = mnist.target
     slc = FullSLCluster(k=10,
-                model_type='gradient_boosting',
-                n_forests=64,
+                model_type='random_forest',
+                n_forests=192,
                 n_trees=2,
-                n_features_to_predict=0.5,
-                max_depth=2, #should be 2 for boosting
+                n_features_to_predict=0.4,
+                max_depth=5, #should be 2 for boosting
                 learning_rate=0.6,
                 using_weights=True,
-                weight_extent=2, # 2 for boosting
+                weight_extent=1.5, # 2 for boosting
                 max_iter=60,
                 n_attempts=10,
                 weight_adjustment=0,
-                eig_extent=5,
+                eig_extent=1,
                 n_jobs=-1)
     assignments = slc.fit_predict(X)
