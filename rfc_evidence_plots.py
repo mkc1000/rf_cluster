3fa5c8480d759a76c8c95179cefb409fef2d8543
Michael Cohen <mkc1000@gmail.com>
2017-07-05 15:00:46 -0700

    add other baseline algorithms

--- rfc_evidence_plots.py
+++ rfc_evidence_plots.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+#rimport seaborn as sns
 import cPickle as pickle
 
 def load_model_data():
@@ -17,24 +17,22 @@ def plot_performance(model_data, kmeans_data):
     d = {'boston': 'Boston Housing', 'breast_cancer' : 'Breast Cancer', 'diabetes' : 'Diabetes', 'iris' : 'Iris'}
     datasets = np.unique([model[0] for model in model_data])
     fig = plt.figure(figsize=(10,6))
-    plt.subplots_adjust(hspace=0.5,top=0.85)
-    title = 'Figures of Merit for Random Forest Clustering and k-Means'
+    plt.subplots_adjust(hspace=0.5) #,top=0.85)
     for i, dataset in enumerate(datasets):
         k_data_rfc = [model[3] for model in model_data if model[0] == dataset]
         wcv_data_rfc = [model[2] for model in model_data if model[0] == dataset]
         k_data_kmeans = [model[3] for model in kmeans_data if model[0] == dataset]
         wcv_data_kmeans = [model[2] for model in kmeans_data if model[0] == dataset]
         ax = fig.add_subplot(2,2,i+1)
-        ax.scatter(k_data_kmeans, wcv_data_kmeans, color='r', label='k-Means')
-        ax.scatter(k_data_rfc, wcv_data_rfc, color='b', label='Random Forest Clustering')
+        ax.scatter(k_data_kmeans, wcv_data_kmeans, color='r', marker='^', label='k-Means')
+        ax.scatter(k_data_rfc, wcv_data_rfc, color='b', label='IRFC')
         if i == 3:
-            plt.legend(loc=(-0.42,1.07),fontsize=11)
+            plt.legend(loc=(-0.28,1.07),fontsize=11)
         plt.title(d[dataset])
     # bigax = fig.add_subplot(1,1,1,frameon=False)
     # bigax.axis('off')
     plt.text(-2.4,-0.06,'Number of Clusters', size=14)
     plt.text(-13,1.1,'Figure of Merit Score', size=15, rotation=90)
-    plt.text(-7.95,1.75,'Performance of k-Means and Random Forest Clustering', size=16)
 
 
 if __name__ == '__main__':
--- test_best.py
+++ test_best.py
@@ -2,12 +2,36 @@ import numpy as np
 from sklearn.datasets import load_boston, load_diabetes, load_iris, load_breast_cancer
 from within_cluster_variance import WCVScore, DevariancedModel
 from compare_algorithms import FullSLCluster, grid_search
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN, AffinityPropagation, AgglomerativeClustering, Birch
 from joblib import Parallel, delayed
 import cPickle as pickle
 
 KMEANS_PARAMS = {
     'model': [KMeans],
+    'dataset' : ['boston','diabetes','iris','breast_cancer'],
+    'n_clusters': [2,3,4,5,6,7,8,9,10]
+}
+
+DBSCAN_PARAMS = {
+    'model': [DBSCAN],
+    'dataset' : ['boston','diabetes','iris','breast_cancer'],
+    'eps': [0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2]
+}
+
+AFFPROP_PARAMS = {
+    'model': [AffinityPropagation],
+    'dataset' : ['boston','diabetes','iris','breast_cancer'],
+    'damping': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+}
+
+AGGLOM_PARAMS = {
+    'model': [AgglomerativeClustering],
+    'dataset' : ['boston','diabetes','iris','breast_cancer'],
+    'n_clusters': [2,3,4,5,6,7,8,9,10]
+}
+
+BIRCH_PARAMS = {
+    'model': [Birch],
     'dataset' : ['boston','diabetes','iris','breast_cancer'],
     'n_clusters': [2,3,4,5,6,7,8,9,10]
 }
@@ -72,12 +96,22 @@ def prepare_tuned_models(select_params_output):
     return params
 
 if __name__ == '__main__':
-    models_to_search = grid_search(SLC_PARAMS)
-    fit_model_output = Parallel(n_jobs=-1)(delayed(fit_model)(model) for model in models_to_search)
-    with open('big_ole_grid_search_output_no_pca.pkl','w') as f:
-        pickle.dump(fit_model_output, f)
-    params_per_dataset = select_params_per_dataset_per_k(fit_model_output)
-    params_to_retry = prepare_tuned_models(params_per_dataset)
-    final_models = Parallel(n_jobs=-1)(delayed(fit_model)(model) for model in params_to_retry)
-    with open('final_models_from_grid_search_no_pca.pkl','w') as f:
-        pickle.dump(final_models, f)
+    ## Make baseline data
+    baseline_models = ['dbscan', 'affprop', 'agglom', 'birch', 'kmeans']
+    baseline_params = [DBSCAN_PARAMS, AFFPROP_PARAMS, AGGLOM_PARAMS, BIRCH_PARAMS, KMEANS_PARAMS]
+    for model, params in zip(baseline_models, baseline_params):
+        models_to_search = grid_search(params)
+        fit_model_output = Parallel(n_jobs=-1)(delayed(fit_model)(model) for model in models_to_search)
+        with open(model + '_baseline_models.pkl','w') as f:
+            pickle.dump(fit_model_output, f)
+
+    # ## Creating the RFCluster stuff
+    # models_to_search = grid_search(SLC_PARAMS)
+    # fit_model_output = Parallel(n_jobs=-1)(delayed(fit_model)(model) for model in models_to_search)
+    # with open('big_ole_grid_search_output_no_pca.pkl','w') as f:
+    #     pickle.dump(fit_model_output, f)
+    # params_per_dataset = select_params_per_dataset_per_k(fit_model_output)
+    # params_to_retry = prepare_tuned_models(params_per_dataset)
+    # final_models = Parallel(n_jobs=-1)(delayed(fit_model)(model) for model in params_to_retry)
+    # with open('final_models_from_grid_search_no_pca.pkl','w') as f:
+    #     pickle.dump(final_models, f)
